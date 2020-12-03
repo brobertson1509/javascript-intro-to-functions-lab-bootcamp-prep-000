0e61d04655f6424993865f403a9f5d79099819fc
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-03 18:00:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function logWhisper(string) {
   logWhisper().toLowerCase()
 
 function sayHiToGrandma(string) {
-if (string.toLowerCase()) {return "I can't hear you!"
- } else if (string.toUpperCase() {return "YES INDEED!"
+if (string.toLowerCase() === string) { return "I can't hear you!"
+ } else if (string.toUpperCase() === string) { return "YES INDEED!"
   } else if (string === "I love you, Grandma.") {return "I love you, too."
 }
   }
