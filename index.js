ae9dac85e909d83d1c2276fa6e96672e809283fe
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-03 17:08:40 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('hello')
+}
+  logShout().toUpperCase()
+
+function logWhisper(string) {
+  console.log('HELLO')
+}
+  logWhisper().toLowerCase()
+
+function sayHiToGrandma(string) {
+  return string.toLowerCase()
+  return string.toUpperCase()
+  }
