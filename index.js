aed142cfab46499ce8403864e06078193a47f4b7
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-03 17:29:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string) {
   var uppercase = "HELLO!"
   var lowercase = "hello!"
   var mixedcase = "I love you, Grandma."
-  
+
   if (string === lowercase) {
     console.log("I can't hear you")
   } else if (string === uppercase) {console.log("YES INDEED!")
